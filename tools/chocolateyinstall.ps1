8368ae114681a0dae18a87fb769fb5f5fe3807c2
ugirirajan <ugirirajan@CORP.AMAZONWORKSPACES.COM>
2019-01-09 11:44:17 +0530

    first commit

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,31 @@
+﻿$packageName = 'baretail'
+$installerType = 'exe'
+$version = '3.50'
+$FROM_LOCATION = Split-Path $MyInvocation.MyCommand.Definition 
+$filename = 'baretail.exe'
+$silentArgs = '/SILENT'
+$PROD_HOME = "C:\BareTail"
+$machine = $false
+$url = ''
+$pathType = 'User'
+
+if ($FROM_LOCATION.Contains('/')) { $url = "$FROM_LOCATION/$filename" }
+else { $url = "$FROM_LOCATION\$filename" }
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" 
+
+Copy-Item $url -Destination $PROD_HOME
+ 
+#Setting path variable instead of using shims. Generate .ignore files for unwanted .exe files
+$files = get-childitem "$PROD_HOME" -include *.exe -recurse
+foreach ($file in $files) {
+    New-Item "$file.ignore" -type file -force | Out-Null
+}
+
+ 
+Install-ChocolateyPath -pathToInstall $PROD_HOME -pathType $pathType
+ 
+Install-ChocolateyDesktopLink -targetFilePath "$PROD_HOME\baretail.exe"
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = 'Stop'
+$script_path = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
+$packageName = 'baretail'
+$version = '3.50'
+$PROD_HOME = "C:\BareTail"
+$filename = "baretail.exe"
+$machine = $false
+$pathType = 'User'
+ 
+$common = $(Join-Path $script_path "common.ps1")
+. $common
+ 
+$packageParameters = $env:chocolateyPackageParameters
+ 
+ 
+Uninstall-Path -pathToUnInstall $PROD_HOME -pathType $pathType
+ 
+if (Test-Path "$PROD_HOME") {
+  Write-Host "Removing $PROD_HOME..."
+  remove-item -Recurse -Force "$PROD_HOME"
+}
+ 
+if (Test-Path "$env:USERPROFILE\Desktop\baretail.exe.lnk") {
+  Write-Host "Removing $env:USERPROFILE\Desktop\baretail.exe.lnk..."
+  remove-item "$env:USERPROFILE\Desktop\baretail.exe.lnk"
+}
